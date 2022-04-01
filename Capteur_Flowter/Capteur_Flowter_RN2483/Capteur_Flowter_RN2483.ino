c7fe8a68a5164de24c47b3b9cc887043203db546
Simon Moinard <simmoinard@gmail.com>
2022-04-01 10:31:54 +0200

    add 3d files & change time to 3min sleep

--- Capteur_Flowter/Capteur_Flowter_RN2483/Capteur_Flowter_RN2483.ino
+++ Capteur_Flowter/Capteur_Flowter_RN2483/Capteur_Flowter_RN2483.ino
@@ -14,21 +14,14 @@ rn2xx3 myLora(loraSerial);
 //TTN
 int RESET_PIN =10;
 #define freqPlan TTN_FP_EU868
-const char *appEui = "70B3D57ED000F8A4";
+const char *appEui = "0000000000000000";
 const char *appKey = "75D677D2EE55359B9F4595E7CC2083DE";
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan, 10); //SF10
 
-//Measure system
-const int wakeUpPin = 2;
-void wakeUp()
-{
-    // Just a handler for the pin interrupt.
-}
 
 void setup()
 {
   //Setup system
-  pinMode(wakeUpPin, INPUT);   
   pinMode(LED_BUILTIN, OUTPUT);
 
   //PARAMETRAGE TTN
@@ -57,7 +50,7 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
   delay(10);                      
   digitalWrite(LED_BUILTIN, LOW);
-  int val = digitalRead(wakeUpPin);
+  int val = digitalRead(A3);
 
   //------- SEND THE VALUE THROUGH TTN ------- 
   debugSerial.println("-- LOOP");
@@ -71,18 +64,9 @@ void loop()
   //------- SLEEP MODE -------
   ttn.sleep(4294967290);  //sleep for 100 000 000 millisec, 1660min, 30hours
   delay(100);
-  for (int i = 0; i <= 1; i++) {
+  for (int i = 0; i < 20; i++) {
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
   }
-  delay(100);
-  debugSerial.println("sleep...");
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(10);                      
-  digitalWrite(LED_BUILTIN, LOW);
-  attachInterrupt(digitalPinToInterrupt(wakeUpPin), wakeUp, CHANGE);
-  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
-  delay(50);
-  detachInterrupt(digitalPinToInterrupt(wakeUpPin)); 
-  debugSerial.println("rebooting...");
+  delay(10);
   ttn.wake();
 }
