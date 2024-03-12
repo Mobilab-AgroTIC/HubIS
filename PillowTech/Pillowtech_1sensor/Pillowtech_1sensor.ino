a57f71b522109e1ba6ab066fd9949b7d46b3844d
Simon Moinard <simmoinard@gmail.com>
2024-03-12 17:26:36 +0100

    commit

--- PillowTech/Pillowtech_1sensor/Pillowtech_1sensor.ino
+++ PillowTech/Pillowtech_1sensor/Pillowtech_1sensor.ino
@@ -2,14 +2,14 @@
 #include "Arduino.h"
 
 //Set these OTAA parameters to match your app/node in TTN
-uint8_t devEui[] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0x06, 0x05 };
-uint8_t appEui[] = { 0x76, 0x15, 0x82, 0x23, 0x77, 0x16, 0x82, 0x50 };
-uint8_t appKey[] = { 0x5F, 0x14, 0x5E, 0xCB, 0x37, 0x95, 0x2A, 0x26, 0xA3, 0x1B, 0xA1, 0xBE, 0x3C, 0x35, 0x29, 0x62 };
+uint8_t appEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t devEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t appKey[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 int temps = 180; // Indiquez dans cette ligne la fréquence d'envoi de données, en secondes. (Ne pas aller plus bas que 3minutes, soit 180sec)
 
 uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
 static uint8_t counter=0;
-uint8_t lora_data[3];
+uint8_t lora_data[2];
 uint8_t downlink ;
 
 ///////////////////////////////////////////////////
@@ -70,15 +70,12 @@ void loop()
   digitalWrite(GPIO7,HIGH);
   delay(1500);
   int sensorValue2 = analogRead(ADC2)/16;
-  int sensorValue3 = analogRead(ADC3)/16;
   digitalWrite(GPIO7,LOW);
   Serial.printf("\nVal 2 : %d\n", sensorValue2);
-  Serial.printf("\nVal 3 : %d\n", sensorValue3);
 
   Serial.printf("\nVoltage : %d\n", voltage);
   lora_data[0] = voltage;
   lora_data[1] = sensorValue2;
-  lora_data[2] = sensorValue3; 
   //Now send the data. The parameters are "data size, data pointer, port, request ack"
   Serial.printf("\nSending packet with counter=%d\n", counter);
   Serial.printf("\nValue 1 to send : %d\n", lora_data[1]);
---
+++ PillowTech/Pillowtech_CubeCell_rawData/Pillowtech_CubeCell_rawData.ino
@@ -0,0 +1,30 @@
+#include "LoRaWanMinimal_APP.h"
+#include "Arduino.h"
+
+
+///////////////////////////////////////////////////
+void setup() {
+  Serial.begin(115200);
+  pinMode(GPIO7,OUTPUT);
+  digitalWrite(GPIO7,LOW);
+}
+
+
+///////////////////////////////////////////////////
+void loop()
+{
+
+  delay(100);
+  uint8_t voltage = getBatteryVoltage()/50; //Voltage in %
+  digitalWrite(GPIO7,HIGH);
+  delay(1500);
+  int sensorValue2 = analogRead(ADC2)/16;
+  int sensorValue3 = analogRead(ADC3)/16;
+  digitalWrite(GPIO7,LOW);
+  
+  Serial.printf("\voltage : %d\n", voltage);
+  Serial.printf("\nVal 2 : %d\n", sensorValue2);
+  Serial.printf("\nVal 3 : %d\n", sensorValue3);
+
+  delay(100);
+}
